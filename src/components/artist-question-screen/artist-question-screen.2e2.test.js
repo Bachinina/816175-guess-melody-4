97d3bec92cdfda85170de472be5cc2546597b5f1
Bachinina <olya.bachinina@yandex.ru>
2020-06-25 15:10:49 +0300

    add question-screen components

---
+++ src/components/artist-question-screen/artist-question-screen.2e2.test.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+import ArtistQuestionScreen from "./artist-question-screen.jsx";
+
+configure({adapter: new Adapter()});
+
+const mock = {
+  question: {
+    type: `artist`,
+    song: {
+      artist: ``,
+      src: ``
+    },
+    answers: [
+      {
+        artist: `one`,
+        picture: `pic-one`,
+      },
+      {
+        artist: `two`,
+        picture: `pic-two`,
+      },
+      {
+        artist: `three`,
+        picture: `pic-three`,
+      },
+    ],
+  }
+};
+
+
+const mockEvent = {
+  preventDefault() {}
+};
+
+it(`Click on user answer should pass to the callback data-object from which this answer was created`, () => {
+  const {question} = mock;
+  const onAnswer = jest.fn();
+  const userAnswer = {
+    artist: `two`,
+    picture: `pic-two`,
+  };
+
+  const screen = shallow(
+      <ArtistQuestionScreen
+        question={question}
+        onAnswer={onAnswer}
+      />
+  );
+
+  const form = screen.find(`form`);
+  const secondInput = form.find(`input`).at(1);
+
+  secondInput.simulate(`change`, mockEvent);
+
+  expect(onAnswer).toHaveBeenCalledTimes(1);
+  expect(onAnswer.mock.calls[0][0]).toMatchObject(question);
+  expect(onAnswer.mock.calls[0][1]).toMatchObject(userAnswer);
+});
